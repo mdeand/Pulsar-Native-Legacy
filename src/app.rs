33efd53909eb47847a95ce72dd8f57f625c7f0e9
Nils Kristians Ramps-Rampans <49234603+SafeShows@users.noreply.github.com>
2025-02-08 23:15:13 +0200

    testing reactivity

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,33 @@
-use gpui::{div, rgb, IntoElement, ParentElement, Render, Styled, ViewContext, VisualContext, WindowContext};
+use std::{cell::RefCell, rc::Rc};
+
+use gpui::{div, rgb, InteractiveElement, IntoElement, ParentElement, Render, Styled, ViewContext, VisualContext, WindowContext};
 
 
-pub struct App{}
+pub struct App{
+    count: Rc<RefCell<i32>>,
+}
 
 impl App {
     pub fn new(cx: &mut WindowContext) -> gpui::View<Self> {
-        cx.new_view(|_| App {})
+        cx.new_view(|_| App { count: Rc::new(RefCell::new(0)) })
     }
 }
 
 impl Render for App {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
-        div().text_color(rgb(0xFFFFFF)).child("testing")
+        let count = self.count.clone();
+        div().children(vec![
+            div()
+                .text_color(rgb(0xFFFFFF))
+                .child(self.count.borrow().to_string()),
+            div()
+                .text_color(rgb(0xFFFFFF))
+                .child("+")
+                .on_mouse_down(gpui::MouseButton::Left, move |_, cx| {
+                    *count.borrow_mut() += 1;
+                    println!("{}", count.borrow());
+                    cx.refresh();
+                }),
+        ])
     }
 }
